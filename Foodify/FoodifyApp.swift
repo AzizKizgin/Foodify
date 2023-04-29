8afeca48921539e14c0aa61687f5a651ae0faaff
Aziz Kızgın <azizkzgn@gmail.com>
2023-04-29 20:43:55 +0300

    Initial Commit

---
+++ Foodify/FoodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodifyApp.swift
+//  Foodify
+//
+//  Created by Aziz Kızgın on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
