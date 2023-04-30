2e7c4648a94466db38377691f9e282acb74079dd
Aziz Kızgın <azizkzgn@gmail.com>
2023-04-30 19:05:11 +0300

    feat: urls added

---
+++ Foodify/Utilities/Urls.swift
@@ -0,0 +1,20 @@
+//
+//  Urls.swift
+//  Foodify
+//
+//  Created by Aziz Kızgın on 30.04.2023.
+//
+
+import Foundation
+
+struct Urls {
+    static let baseUrl = "https://www.themealdb.com/api/json/v1/1"
+    static let search = "\(baseUrl)/search.php"
+    static let detailById = "\(baseUrl)/lookup.php"
+    static let random = "\(baseUrl)/random.php"
+    static let categories = "\(baseUrl)/categories.php"
+    static let categoryList = "\(baseUrl)/list.php?c=list"
+    static let areaList = "\(baseUrl)/list.php?a=list"
+    static let ingredientList = "\(baseUrl)/list.php?i=list"
+    static let filter = "\(baseUrl)/filter.php"
+}
