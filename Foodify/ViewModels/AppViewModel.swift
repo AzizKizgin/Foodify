ee85b3a8e612825ec5b5ecdf536a97e800b51d06
Aziz Kızgın <azizkzgn@gmail.com>
2023-05-03 21:52:44 +0300

    feat: new images added

---
+++ Foodify/ViewModels/AppViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  HomeViewModel.swift
+//  Foodify
+//
+//  Created by Aziz Kızgın on 29.04.2023.
+//
+
+import Foundation
+
+class AppViewModal: ObservableObject {
+    @Published var categories: [Category] = []
+    @Published var meals: [Meal] = []
+    @Published var mealsByCategory: [MealByCategory] = []
+    @Published var meal: Meal? = nil
+    private let categoryService = CategoryApiService()
+    private let mealService = MealApiService()
+    func fetchCategories(){
+        categoryService.fetchCategories{
+            result,error  in
+            guard let categories = result else{
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async {
+                self.categories = categories
+            }
+        }
+    }
+    
+    func searchMeal(name:String){
+        mealService.searchMealByName(name: name){
+            result, error in
+            guard let meals = result else{
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async {
+                self.meals = meals
+            }
+        }
+    }
+    
+    func getMealById(id:Int){
+        mealService.searchMealById(id:id){
+            result, error in
+            guard let meal = result else{
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async {
+                self.meal = meal
+            }
+        }
+    }
+    
+    func getMealByCategory(category:String){
+        mealService.searchMealByCategory(category: category){
+            result ,error in
+            guard let meals = result else {
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async {
+                self.mealsByCategory = meals
+            }
+        }
+    }
+    
+}
---
+++ Foodify/Views/Home/Components/CategoryList.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryList.swift
+//  Foodify
+//
+//  Created by Aziz Kızgın on 2.05.2023.
+//
+
+import SwiftUI
+
+struct CategoryList: View {
+    let categoryList: [CategoryListItem]
+    var body: some View {
+        ForEach(categoryList, id:\.id){
+            category in
+            NavigationLink(destination: Meals(text:"\(category.name)")){
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20,style: .continuous)
+                        .fill(category.color)
+                    VStack(spacing: 8){
+                        Text(category.name)
+                            .font(.system(size: 32))
+                            .foregroundColor(.black)
+                        Text("\(category.mealCount) recipe")
+                            .foregroundColor(.black)
+                        Spacer()
+                        Image(category.name.lowercased())
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame( width: 175)
+                    }.padding()
+                }
+                .frame(height:250)
+                 .padding(.horizontal)
+                .padding(.vertical,10)
+            }
+        }
+    }
+}
+
